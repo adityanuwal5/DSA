fda702b0f973bea9d9d5664036e474adc245451a
Aditya Nuwal <adityanuwal@Adityas-MacBook-Air-2.local>
2026-09-16 16:07:59 +0530

    Leetcode Problem Statement 1

--- leetcode_q1.py
+++ leetcode_q1.py
@@ -1,9 +1,24 @@
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         num_dict = {}
+#         for i, num in enumerate(nums):
+#             complement = target - num
+#             if complement in num_dict:
+#                 return (num_dict[complement], i)
+#             num_dict[num] = i
+#         return None
+
+# Another approach using two pointers
 class Solution(object):
     def twoSum(self, nums, target):
         num_dict = {}
-        for i, num in enumerate(nums):
-            complement = target - num
+        # Loop through indices from 0 to the length of the list
+        for i in range(len(nums)):
+            num = nums[i]  # Fetch the value manually using the index
+            complement = target - num    
             if complement in num_dict:
                 return (num_dict[complement], i)
             num_dict[num] = i
         return None
+
+
